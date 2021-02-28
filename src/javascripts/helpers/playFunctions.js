4b65d684759d399057fcb56a62c00af1ba81b0b6
Katy-fry <katyvfry@gmail.com>
2021-02-28 16:36:24 -0600

    added readme and personal stretch goals

--- src/javascripts/helpers/playFunctions.js
+++ src/javascripts/helpers/playFunctions.js
@@ -12,15 +12,21 @@ const minusFun = () => {
 
 const playMessage = 'GET SOME REST';
 
+const funIcon = () => {
+  const funIcons = ['<i class="fas fa-guitar text-center"></i>', '<i class="fas fa-camera-retro text-center"></i>', '<i class="fas fa-book-dead text-center"></i>', '<i class="fas fa-chess text-center"></i>'];
+  const randomActivity = funIcons[Math.floor(Math.random() * funIcons.length)];
+  return randomActivity;
+};
+
 const haveFun = (e) => {
   const targetId = e.target.id;
 
   if (targetId === 'superFun') {
     document.querySelector('#funScore').innerHTML = `<h4>${addFun()}</h4>`;
-    document.querySelector('#funArea').innerHTML = '<i class="fas fa-book-dead"></i>';
+    document.querySelector('#funArea').innerHTML = funIcon();
   } else if (targetId === 'slightlyFun') {
     document.querySelector('#funScore').innerHTML = `<h4>${minusFun()}</h4>`;
-    document.querySelector('#funArea').innerHTML = '<i class="fas fa-chess"></i>';
+    document.querySelector('#funArea').innerHTML = funIcon();
   }
 
   if (fun >= 100) {
